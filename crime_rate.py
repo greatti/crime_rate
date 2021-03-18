a0d1400c1f803fc66130493edfec8c28ef23d686
greatti <greattibrenno@gmail.com>
2021-03-18 13:55:56 -0300

    Update crime_rate.py
    Reset the index of df;
    Create a copy of df called dfc;
    Delete 'ANO' column from dfc;
    Set 'INVEST' as index of dfc;
    Sort the dfc index;
    Reset the dfc index;
    Plot 'Crime vs INVEST' and 'Bolsas vs INVEST'.

--- crime_rate.py
+++ crime_rate.py
@@ -28,3 +28,26 @@ print(len(df.columns))
 df.plot(kind = 'line', subplots = True, grid = True, title = 'Dados por Ano', 
         layout = (3,1), sharex = True, legend = True, 
         colormap = 'cividis')
+
+df = df.reset_index()
+dfc = df.copy()
+del dfc['ANO']
+
+
+dfc = dfc.set_index('INVEST')
+dfc = dfc.sort_index()
+dfc = dfc.reset_index()
+
+dfc.plot.line(y = 'Crim/100mil', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
+             ylabel = 'Criminalidade a cada  100mil Habitantes',
+             grid = True,
+             colormap = 'viridis'
+             )
+
+dfc.plot.line(y = 'Bolsas_Atleta', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na quantidade de bolsas atleta fornecidas',
+             ylabel = 'Numero de Bolsas Atleta fornecidas',
+             grid = True,  
+             colormap = 'plasma'
+             )
