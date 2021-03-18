b798db358afc85e4fe90ada3c755c98c8669cf7c
greatti <greattibrenno@gmail.com>
2021-03-18 13:51:50 -0300

    Update
    Update the data;
    Set an style with seaborn;
    Import the data with pandas;
    Rename a column;
    Define a index.

--- crime_rate.py
+++ crime_rate.py
@@ -9,3 +9,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+sns.set_style('white') #vamos mudar o estilo
+
+df = pd.read_csv('C:/Users/great/Desktop/piton/matplotlib_seaborn/data.csv')
+
+df= df.rename(columns = {'Invest. Esp' : 'INVEST'})
+df.ANO = pd.to_numeric(df.ANO , errors='coerce')
+df.INVEST = pd.to_numeric(df.INVEST , errors='coerce')
+
+df = df.set_index('ANO')
+print(df)
+print(df.columns)
