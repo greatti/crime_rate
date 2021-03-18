5d3d3d9700a5eb239e77492da1898ad0fcf3b78c
greatti <greattibrenno@gmail.com>
2021-03-18 13:44:12 -0300

    Add 2 directories and python file
    Add images;
    Add data;
    Add python file;
    Add some libraries to the file.

---
+++ crime_rate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 19:20:49 2021
+
+@author: great
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
